0dcdf7ed17f9afb984bfb03d6809f2ca9654ae4b
rahul soni <rahuledumanag@gmail.com>
2024-05-16 16:47:22 +0530

    update navbar links

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -11,9 +11,8 @@ $SubMenuModel = SubMenu::where(['SubMen_Reg_Id' => $clientId])
     ->orderBy('SubMen_SerialOrder', 'asc')
     ->get();
 $SocialLinkModel = Page::leftJoin('tbl_pagecategory', 'tbl_page.Pag_PagCat_Id', '=', 'tbl_pagecategory.PagCat_Id')->where('Pag_Reg_Id', '=', $clientId)->where('Pag_Status', '=', '0')->where('tbl_pagecategory.PagCat_Name', 'SocialLink')->orderBy('Pag_SerialOrder', 'asc')->get();
-//    dd($MenuModel);
 $AnnouncementModel = Page::leftJoin('tbl_pagecategory', 'tbl_page.Pag_PagCat_Id', '=', 'tbl_pagecategory.PagCat_Id')->where('Pag_Reg_Id', '=', $clientId)->where('tbl_pagecategory.PagCat_Name', 'Announcement')->where('Pag_Status', '=', '0')->orderBy('tbl_page.Pag_CreatedDate', 'asc')->get();
-?> <!--===== HEADER TOP =====-->
+?>
 <div id="header-top">
     <div class="container">
         <div class="row">
@@ -32,12 +31,11 @@ $AnnouncementModel = Page::leftJoin('tbl_pagecategory', 'tbl_page.Pag_PagCat_Id'
         </div>
     </div>
 </div>
-<!--===== #/HEADER TOP =====-->
-<!--===== HEADER BOTTOM =====-->
 <div id="header-bottom">
     <div class="container">
         <div class="row">
-            <div class="col-md-2 hidden-xs hidden-sm"><a href="/"><img src="{{ env('Web_CommonURl') }}{{ $WebInfoModel->WebInf_HeaderLogo ?? 'N/A' }}"
+            <div class="col-md-2 hidden-xs hidden-sm"><a href="/"><img
+                        src="{{ env('Web_CommonURl') }}{{ $WebInfoModel->WebInf_HeaderLogo ?? 'N/A' }}"
                         alt="logo" /></a></div>
             <div class="col-md-10 col-sm-12 col-xs-12">
                 <div class="get-tuch text-left top20">
@@ -51,7 +49,8 @@ $AnnouncementModel = Page::leftJoin('tbl_pagecategory', 'tbl_page.Pag_PagCat_Id'
                         </li>
                     </ul>
                 </div>
-                <div class="get-tech-line top20"><img src="{{ asset('assets/frontend/images/get-tuch-line.png') }}" alt="line" /></div>
+                <div class="get-tech-line top20"><img src="{{ asset('assets/frontend/images/get-tuch-line.png') }}"
+                        alt="line" /></div>
                 <div class="get-tuch text-left top20">
                     <i class="icon-icons74"></i>
                     <ul>
@@ -63,7 +62,8 @@ $AnnouncementModel = Page::leftJoin('tbl_pagecategory', 'tbl_page.Pag_PagCat_Id'
                         </li>
                     </ul>
                 </div>
-                <div class="get-tech-line top20"><img src="{{ asset('assets/frontend/images/get-tuch-line.png') }}" alt="line" /></div>
+                <div class="get-tech-line top20"><img src="{{ asset('assets/frontend/images/get-tuch-line.png') }}"
+                        alt="line" /></div>
                 <div class="get-tuch text-left top20">
                     <i class=" icon-icons142"></i>
                     <ul>
@@ -79,159 +79,82 @@ $AnnouncementModel = Page::leftJoin('tbl_pagecategory', 'tbl_page.Pag_PagCat_Id'
         </div>
     </div>
 </div>
-<!--===== #/HEADER BOTTOM =====-->
-<!--===== NAV-BAR =====-->
 <nav class="navbar navbar-default navbar-sticky bootsnav">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
                 <div class="social-icons text-right">
                     <ul class="socials">
-                     @foreach ($SocialLinkModel as $model)
-                        <li><a href="{{ $model->Pag_URL }}" target="_blank">{!! $model->Pag_Image !!}
-                            </a></li>
-                    @endforeach
+                        @foreach ($SocialLinkModel as $model)
+                            <li><a href="{{ $model->Pag_URL }}" target="_blank">{!! $model->Pag_Image !!}
+                                </a></li>
+                        @endforeach
                         <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                      
                     </ul>
                 </div>
-                <!-- Start Header Navigation -->
                 <div class="navbar-header">
                     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
                         <i class="fa fa-bars"></i></button>
                     <a class="navbar-brand sticky_logo" href="/">
-                        <img src="{{ env('Web_CommonURl') }}{{ $WebInfoModel->WebInf_HeaderLogo ?? 'N/A' }}" class="logo"
-                            alt=""></a>
+                        <img src="{{ env('Web_CommonURl') }}{{ $WebInfoModel->WebInf_HeaderLogo ?? 'N/A' }}"
+                            class="logo" alt=""></a>
                 </div>
-                <!-- End Header Navigation -->
-                <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse" id="navbar-menu">
                     <ul class="nav navbar-nav" data-in="fadeInDown" data-out="fadeOutUp">
-                        <li >
-                            <a href="{{ URL::to('/') }}">Home</a>
+                        @foreach ($MenuModel as $key => $value)
+                            <li class="" @if ($key !== 0) style="" @endif>
+                                @if ($value->Men_SubMenuExists == 'on')
+                                    @php
+                                        $subMenuItems = $SubMenuModel->where('SubMen_Men_Id', $value->Men_Id);
+                                    @endphp
+
+
+                                    @if ($subMenuItems->isNotEmpty())
+                            <li class="dropdown">
+                                @php
+                                    $menuUrl = $value->Men_URL
+                                        ? URL::to($value->Men_URL)
+                                        : route('showDetails', ['type' => 'menu', 'id' => encodeId($value->Men_Id)]);
+                                @endphp
+
+                                <a class=" dropdown-toggle" href="{{ $menuUrl }}"
+                                    id="navbarDropdown{{ $value->Men_Id }}" role="button" data-toggle="dropdown"
+                                    aria-haspopup="true" aria-expanded="false">
+                                    {{ $value->Men_Name }}
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li class="dropdown">
+                                        @foreach ($subMenuItems as $submenu)
+                                            <a class="dropdown-item"
+                                                href="{{ $submenu->SubMen_URL ? URL::to($submenu->SubMen_URL) : route('showSubMenuDetails', ['type' => 'submenu', 'id' => encodeId($submenu->SubMen_Id)]) }}
+                                                         ">{{ $submenu->SubMen_Name }}</a>
+                                        @endforeach
+                                    </li>
+
+                                </ul>
+                            </li>
+                        @else
+                            <li class="">
+                                <a class=""
+                                    href="{{ $value->Men_URL ? URL::to($value->Men_URL) : route('showDetails', ['type' => 'menu', 'id' => encodeId($value->Men_Id)]) }}">
+                                    {{ $value->Men_Name }}
+                                </a>
+                            </li>
+                        @endif
+                    @else
+                        <li class="">
+                            <a class=""
+                                href="{{ $value->Men_URL ? URL::to($value->Men_URL) : route('showDetails', ['type' => 'menu', 'id' => encodeId($value->Men_Id)]) }}">
+                                {{ $value->Men_Name }}
+                            </a>
                         </li>
-                        <li >
-                            <a href="{{ URL::to('/about') }}">About</a>
+                        @endif
                         </li>
-                        <li class="dropdown">
-                            <a href="#." class="dropdown-toggle" data-toggle="dropdown">Properties</a>
-                            <ul class="dropdown-menu">
-                                <li class="dropdown">
-                                    <a href="listing.html">Property Listing</a>
-                                </li>
-                                <li class="dropdown">
-                                    <a href="propertydetails.html">Property Details</a>
-                                </li>
-                                <li class="dropdown">
-                                    <a href="submitproperty.html">Creat New Property</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="dropdown">
-                            <a href="#." class="dropdown-toggle" data-toggle="dropdown">Pages</a>
-                            <ul class="dropdown-menu">
-                                <li class="dropdown">
-                                    <a href="{{ URL::to('/faqs') }}">FAQ's</a>
-                                </li>
-                                 
-                                <li><a href="{{ URL::to('/testimonial') }}">Testimonial</a></li>
-                                <li><a href="{{ URL::to('/calculator') }}">Loan Calculate</a></li>
-                            </ul>
-                        </li>
-                        <li class="dropdown megamenu-fw">
-                            <a href="#." class="dropdown-toggle" data-toggle="dropdown">Showcase</a>
-                            <ul class="dropdown-menu megamenu-content" role="menu">
-                                <li>
-                                    <div class="row">
-                                        <div class="col-menu col-md-2">
-                                            <h5 class="title">Showcase List</h5>
-                                            <div class="content">
-                                                <ul class="menu-col">
-                                                    <li><a href="#">Properties List</a></li>
-                                                    <li><a href="#">Single Property</a></li>
-                                                    <li><a href="#">Search by City</a></li>
-                                                    <li><a href="#">Search by Category</a></li>
-                                                    <li><a href="#">Search by Type</a></li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                        <div class="col-menu col-md-2">
-                                            <h5 class="title">Showcase List</h5>
-                                            <div class="content">
-                                                <ul class="menu-col">
-                                                    <li><a href="#">Properties List</a></li>
-                                                    <li><a href="#">Single Property</a></li>
-                                                    <li><a href="#">Search by City</a></li>
-                                                    <li><a href="#">Search by Category</a></li>
-                                                    <li><a href="#">Search by Type</a></li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                        <div class="col-menu col-md-8">
-                                            <div class="row">
-                                                <div id="nav_slider" class="owl-carousel">
-                                                    <div class="item">
-                                                        <div class="image bottom15">
-                                                            <img src="{{ asset('assets/frontend/images/nav-slider1.jpg') }}" alt="Featured Property">
-                                                            <span class="nav_tag yellow text-uppercase">for rent</span>
-                                                        </div>
-                                                        <h4><a href="#.">Park Avenue Apartment</a></h4>
-                                                        <p>Towson London, MR 21501</p>
-                                                    </div>
-                                                    <div class="item">
-                                                        <div class="image bottom15">
-                                                            <img src="{{ asset('assets/frontend/images/nav-slider2.jpg') }}" alt="Featured Property">
-                                                            <span class="nav_tag yellow text-uppercase">for rent</span>
-                                                        </div>
-                                                        <h4><a href="#.">Park Avenue Apartment</a></h4>
-                                                        <p>Towson London, MR 21501</p>
-                                                    </div>
-                                                    <div class="item">
-                                                        <div class="image bottom15">
-                                                            <img src="{{ asset('assets/frontend/images/nav-slider3.jpg') }}" alt="Featured Property">
-                                                            <span class="nav_tag yellow text-uppercase">for rent</span>
-                                                        </div>
-                                                        <h4><a href="#.">Park Avenue Apartment</a></h4>
-                                                        <p>Towson London, MR 21501</p>
-                                                    </div>
-                                                    <div class="item">
-                                                        <div class="image bottom15">
-                                                            <img src="{{ asset('assets/frontend/images/nav-slider1.jpg') }}" alt="Featured Property">
-                                                            <span class="nav_tag yellow text-uppercase">for rent</span>
-                                                        </div>
-                                                        <h4><a href="#.">Park Avenue Apartment</a></h4>
-                                                        <p>Towson London, MR 21501</p>
-                                                    </div>
-                                                    <div class="item">
-                                                        <div class="image bottom15">
-                                                            <img src="{{ asset('assets/frontend/images/nav-slider2.jpg') }}" alt="Featured Property">
-                                                            <span class="nav_tag yellow text-uppercase">for rent</span>
-                                                        </div>
-                                                        <h4><a href="#.">Park Avenue Apartment</a></h4>
-                                                        <p>Towson London, MR 21501</p>
-                                                    </div>
-                                                    <div class="item">
-                                                        <div class="image bottom15">
-                                                            <img src="{{ asset('assets/frontend/images/nav-slider3.jpg') }}" alt="Featured Property">
-                                                            <span class="nav_tag yellow text-uppercase">for rent</span>
-                                                        </div>
-                                                        <h4><a href="#.">Park Avenue Apartment</a></h4>
-                                                        <p>Towson London, MR 21501</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="dropdown">
-                            <a href="{{ URL::to('/contact') }}">Contact Us</a>
-                        </li>
+                        @endforeach
+
                     </ul>
                 </div>
             </div>
         </div>
     </div>
 </nav>
-<!--===== #/NAV-BAR =====-->

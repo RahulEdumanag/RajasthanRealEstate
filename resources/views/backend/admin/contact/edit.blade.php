d8e4027e5eed025f509eb7b5b828ed24927a339a
rahul soni <rahuledumanag@gmail.com>
2024-05-22 10:38:50 +0530

    update admin contact edit file

--- resources/views/backend/admin/contact/edit.blade.php
+++ resources/views/backend/admin/contact/edit.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Edit')
 @section('content')
     <div class="container-fluid">
-          <div class="card-header d-flex justify-content-between align-items-center">
+        <div class="card-header d-flex justify-content-between align-items-center">
             <h5 class="py-3">
                 <span class="text-muted fw-light">Contact/</span> View Contact
             </h5>
@@ -22,54 +22,52 @@
                             {{ method_field('PATCH') }}
                             <div class="row g-3">
                                 @if ($model->Con_PId)
-                                    <div class="col-sm-6">
-                                        <label for="Name" class="form-label">Property Title</label>
-                                        <input type="text" class="form-control" id="Con_PId" name="Con_PId"
-                                            value="{{ optional($model->property)->PTitle }}" placeholder="" readonly>
+                                    <div class="form-group col-lg-6 ">
+                                        <label class="form-label">Property Title:</label>
+                                        <p>{{ optional($model->property)->PTitle }}</p>
                                     </div>
                                 @endif
-
                                 @if ($model->Con_ConCat_Id)
-                                <div class="col-sm-6">
-                                    <label for="Name" class="form-label">Contact Category</label>
-                                    <input type="text" class="form-control" id="Con_Name" name="Con_Name"
-                                        value="{{ $model->Con_ConCat_Id ?? 'N/A' }}" placeholder="" readonly>
-                                </div>
-                                @endif
-
-                                <div class="col-sm-6">
-                                    <label for="Name" class="form-label">Name</label>
-                                    <input type="text" class="form-control" id="Con_Name" name="Con_Name"
-                                        value="{{ old('Con_Name', $model->Con_Name) }}" placeholder="" readonly>
-                                </div>
-                                <div class="col-sm-6">
-                                    <label for="Email" class="form-label">Email</label>
-                                    <input type="text" class="form-control" id="Con_Email" name="Con_Email"
-                                        value="{{ old('Con_Email', $model->Con_Email) }}" placeholder="" readonly>
-                                </div>
-                                <div class="col-sm-6">
-                                    <label for="Number" class="form-label">Number</label>
-                                    <input type="text" class="form-control" id="Con_Number" name="Con_Number"
-                                        value="{{ old('Con_Number', $model->Con_Number) }}" placeholder="" readonly>
-                                </div>
-                                <div class="col-sm-6">
-                                    <label for="Message" class="form-label">Message</label>
-                                    <textarea class="form-control" id="Con_Desc" name="Con_Desc" placeholder="" readonly>{{ old('Con_Desc', $model->Con_Desc) }}</textarea>
-                                </div>
-
-                                @if ($model && $model->Con_ConCat_Id !== null)
-                                    <div class="col-sm-6">
-                                        <label for="Number" class="form-label">Career</label>
-                                        <input type="text" class="form-control" id="Con_ConCat_Id" name="Con_ConCat_Id"
-                                            placeholder="" readonly
-                                            @if ($model->Con_ConCat_Id == 1) value="Internship"
-            @elseif ($model->Con_ConCat_Id == 0) value="Become a Partner" @endif>
+                                    <div class="form-group col-lg-6">
+                                        <label class="form-label">Contact Category:</label>
+                                        <p> {{ $model->Con_ConCat_Id ?? 'N/A' }}</p>
                                     </div>
                                 @endif
-
-
-
-
+                                <div class="form-group col-lg-6">
+                                    <label class="form-label">Name:</label>
+                                    <p>{{ old('Con_Name', $model->Con_Name) }}</p>
+                                </div>
+                                <div class="form-group col-lg-6">
+                                    <label class="form-label">Email:</label>
+                                    <p>{{ old('Con_Email', $model->Con_Email) }}</p>
+                                </div>
+                                <div class="form-group col-lg-6">
+                                    <label class="form-label">Number:</label>
+                                    <p>{{ old('Con_Number', $model->Con_Number) }}</p>
+                                </div>
+                                <div class="form-group col-lg-6">
+                                    <label class="form-label">Message:</label>
+                                    <p>{{ old('Con_Desc', $model->Con_Desc) }}</p>
+                                </div>
+                                @if ($model && $model->Con_ConCat_Id !== null)
+                                    <!-- <div class="col-sm-6">
+                                            <label for="Number" class="form-label">Career</label>
+                                            <input type="text" class="form-control" id="Con_ConCat_Id" name="Con_ConCat_Id"
+                                                placeholder="" readonly
+                                                @if ($model->Con_ConCat_Id == 1) value="Internship"
+                                                    @elseif ($model->Con_ConCat_Id == 0) value="Become a Partner" @endif>
+                                        </div> -->
+                                    <div class="form-group col-lg-6">
+                                        <label class="form-label">Career:</label>
+                                        <p>
+                                            @if ($model->Con_ConCat_Id == 1)
+                                                value="Internship"
+                                            @elseif ($model->Con_ConCat_Id == 0)
+                                                value="Become a Partner"
+                                            @endif
+                                        </p>
+                                    </div>
+                                @endif
                                 <div class="col-sm-6">
                                     @if ($model->Con_Attachment)
                                         <label for="Con_Attachment" class="form-label">CV</label>
@@ -79,7 +77,6 @@
                                         </div>
                                     @endif
                                 </div>
-
                             </div>
                         </form>
                     </div>
